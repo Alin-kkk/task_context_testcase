7d3438db1a05047c5497f82aa2e611ae8c83dffd
alin-kk <65072397+alin-kk@users.noreply.github.com>
2021-12-17 19:08:09 +0800

    Add files via upload

---
+++ testcase/src/testcase1/views/TestView.java
@@ -0,0 +1,64 @@
+package testcase1.views;
+
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.*;
+import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.jface.action.*;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.*;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.SWT;
+import javax.inject.Inject;
+
+
+/**
+ * @stereotype BOUNDARY COMMANDER 
+ */
+public class TestView extends ViewPart {
+
+	/**
+	 * The ID of the view as specified by the extension.
+	 */
+	public static final String ID = "testcase1.views.SampleView";
+
+	@Inject IWorkbench workbench;
+	
+	private TableViewer viewer;
+	 
+	/**
+	 * @stereotype COMMAND COLLABORATOR 
+	 */
+	@Override
+	public void createPartControl(Composite parent) {
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		
+		viewer.setContentProvider(ArrayContentProvider.getInstance());
+		viewer.setInput(new String[] { "One", "Two", "Three" });
+
+		// Create the help context id for the viewer's control
+		workbench.getHelpSystem().setHelp(viewer.getControl(), "testcase1.viewer");
+		getSite().setSelectionProvider(viewer);
+		showMessage(" ");
+	}
+
+
+	/**
+	 * @stereotype COLLABORATOR 
+	 */
+	private void showMessage(String message) {
+		MessageDialog.openInformation(
+			viewer.getControl().getShell(),
+			"Sample View",
+			message);
+	}
+
+	/**
+	 * @stereotype SET 
+	 */
+	@Override
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+}
